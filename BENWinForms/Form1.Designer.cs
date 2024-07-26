95ffe1b49d600a49779e1ef3407138b380451889
Ben <m055054@msn.com>
2024-07-26 09:37:08 +0800

    HW3

--- BENWinForms/Form1.Designer.cs
+++ BENWinForms/Form1.Designer.cs
@@ -28,12 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            fileSystemWatcher1 = new FileSystemWatcher();
+            label3 = new Label();
+            button1 = new Button();
+            label4 = new Label();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 19);
+            label1.TabIndex = 0;
+            label1.Text = "帳號 :";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(30, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 19);
+            label2.TabIndex = 1;
+            label2.Text = "密碼 :";
+            // 
+            // fileSystemWatcher1
+            // 
+            fileSystemWatcher1.EnableRaisingEvents = true;
+            fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(30, 124);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 19);
+            label3.TabIndex = 2;
+            label3.Text = "說明 :";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(30, 227);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "儲存";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(30, 161);
+            label4.Name = "label4";
+            label4.Size = new Size(80, 19);
+            label4.TabIndex = 4;
+            label4.Text = "密碼強度 : ";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 19;
+            listBox1.Location = new Point(286, 43);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(502, 213);
+            listBox1.TabIndex = 5;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(82, 35);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(198, 27);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(82, 78);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(197, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(82, 116);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(197, 27);
+            textBox3.TabIndex = 8;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(150, 227);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 9;
+            button2.Text = "移除";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(label4);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "帳號管理程式";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)fileSystemWatcher1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private FileSystemWatcher fileSystemWatcher1;
+        private ListBox listBox1;
+        private Label label4;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button2;
     }
 }
--- BENWinForms/Form1.cs
+++ BENWinForms/Form1.cs
@@ -1,10 +1,105 @@
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace BENWinForms
 {
     public partial class Form1 : Form
     {
+        private List<string> accountInfoList = new List<string>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string account = textBox1.Text;
+            string password = textBox2.Text;
+            string description = textBox3.Text;
+            if (string.IsNullOrWhiteSpace(account) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(description))
+            {
+                MessageBox.Show("帳號、密碼和說明欄位均不得為空白！");
+                return;
+            }
+            string strength = CheckPasswordStrength(password);
+            string info = $"帳號: {account}, 密碼:{password}, 說明: {description}, 密碼強度: {strength} ";
+            listBox1.Items.Add(info);
+            label4.Text = "密碼強度: " + strength;
+
+
+            // 清空 TextBox
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            
+        }
+        private string CheckPasswordStrength(string password)
+        {
+            bool hasUpperCase = false;
+            bool hasLowerCase = false;
+            bool hasDigit = false;
+            bool hasSpecialChar = false;
+            bool isLongEnough = password.Length >= 8;
+
+            // 特殊符號的檢查
+            string specialChars = "!@#$%^&*";
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c)) hasUpperCase = true;
+                else if (char.IsLower(c)) hasLowerCase = true;
+                else if (char.IsDigit(c)) hasDigit = true;
+                else if (specialChars.Contains(c)) hasSpecialChar = true;
+            }
+
+            int criteriaCount = 0;
+
+            if (hasUpperCase) criteriaCount++;
+            if (hasLowerCase) criteriaCount++;
+            if (hasDigit) criteriaCount++;
+            if (hasSpecialChar) criteriaCount++;
+            if (isLongEnough) criteriaCount++;
+
+            switch (criteriaCount)
+            {
+                case 5:
+                    return "強";
+                case 4:
+                case 3:
+                    return "中";
+                default:
+                    return "弱";
+            }
+        }
+        
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+            if (selectedIndex == -1)
+            {
+                MessageBox.Show("請選擇一個帳號");
+                return;
+            }
+
+            listBox1.Items.RemoveAt(selectedIndex);
+
+            MessageBox.Show("移除成功!!!");
         }
     }
 }
